1e058412e6fb0005c24a548c13b27b9393bb8a40
Pau <49660697+pauchiner@users.noreply.github.com>
2022-04-15 16:16:02 +0200

    Version 0.9
    Now the calculator works 🎉✨

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
-//const operationButtons = document.querySelectorAll('[data-operation]');
-//const equalsButton = document.querySelector('[data-equals]');
-//const allClearButton = document.querySelector('[data-all-clear]');
-//const previousOperandTextElement = document.getElementById('previousValueText');
-//const currentOperandTextElement = document.getElementById('currentValueText');
+//References
+const numberButtons = Array.from(document.getElementsByClassName('button-number'));
+const operatorButtons = Array.from(document.getElementsByClassName('button-operator'));
+const equalsButton = document.getElementById('button-equals');
+const clearButton = document.getElementById('button-clear');
+const previousOperandTextElement = document.getElementById('previousValueText');
+const currentOperandTextElement = document.getElementById('currentValueText');
 
-window.onload = function main() {
-    var numberButtons = Array.from(document.getElementsByClassName('button-number'));
-    
-    var previousOperandTextElement = document.getElementById('#previousValueText');
-    var currentOperandTextElement = document.getElementById('currentValueText');
+//Variables
+let operationHasOperator = false;
 
-   numberButtons.forEach(button => {
-       button.addEventListener('click', addNumber(button, currentOperandTextElement), false);
-   })
-   
-   console.log(numberButtons);
+function clear() {
+    previousOperandTextElement.innerText = "";
+    currentOperandTextElement.innerText = "0";
 }
 
-numberButtons.forEach(button => {
-    button.addEventListener('onclick', addNumber(button, currentOperandTextElement), false);
+function addNumber(element) {
+    if(currentOperandTextElement.innerText == "0") {
+        currentOperandTextElement.innerText = "";
+    }
+    currentOperandTextElement.innerText += element.innerText;
+}
+
+function addOperator(element) {
+    if(currentOperandTextElement.innerText != "0" && !operationHasOperator) {
+        var operation = currentOperandTextElement.innerText;
+        operation += " " + element.innerText + " ";
+        operationHasOperator = true;
+        previousValueText.innerText = operation;
+        currentOperandTextElement.innerText = 0;
+    }
+
+    if(!operationHasOperator) {
+        previousOperandTextElement.innerText += " " + element.innerText + " ";
+    }
+
+    if(operationHasOperator && currentOperandTextElement.innerText != "0") {
+        calculate(false);
+        previousOperandTextElement.innerText += " " + element.innerText + " ";
+        operationHasOperator = true;
+    }
+
+}
+
+function calculate(equalsButton = true) {
+    if(currentOperandTextElement.innerText != 0 && previousOperandTextElement.innerText != 0) {
+        var operation = previousOperandTextElement.innerText + currentOperandTextElement.innerText;
+        if(equalsButton) {
+            currentOperandTextElement.innerText = eval(operation);
+            previousOperandTextElement.innerText = "";
+        }
+        else {
+            currentOperandTextElement.innerText = "0";
+            previousOperandTextElement.innerText = eval(operation);
+        }
+        operationHasOperator = false;
+    }
+}
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        addOperator(button)
+    });
 })
 
-function addNumber(element, currentOperandTextElement) {
-    currentOperandTextElement.innerText = element.innerText;
-    console.log(element);
-}
+numberButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        addNumber(button)
+    });
+})
+
+equalsButton.addEventListener('click', function() {
+    calculate();
+})
+
+clearButton.addEventListener('click', function() {
+    clear();
+})
