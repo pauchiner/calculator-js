9020b4a330f128c8f2ef123ee3e6abee1bc2fd1a
pauchiner <pauchiner@users.noreply.github.com>
2022-05-23 15:12:16 +0000

    Prettified Code!

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # calculator-js
+
 <a href="https://github.com/pauchiner/Scram-Timming/blob/main/LICENSE"><img src="https://img.shields.io/badge/license-MIT-informational" alt="License"></a>
 
- a simple javascript calculator.
- 
+a simple javascript calculator.
+
 [Give me a preview!](https://pauchiner.github.io/calculator-js/)
- 
- ⚠️ If you find any problem do not hesitate to contact me through the [Issues](https://github.com/pauchiner/calculator-js/issues/new).
+
+⚠️ If you find any problem do not hesitate to contact me through the [Issues](https://github.com/pauchiner/calculator-js/issues/new).
--- index.js
+++ index.js
@@ -9,15 +9,21 @@ class Calculator {
   }
 
   clear() {
-    if(this.currentOperand.innerText != '0' || this.previousOperand.innerText != '') {
+    if (
+      this.currentOperand.innerText != "0" ||
+      this.previousOperand.innerText != ""
+    ) {
       //Show empty trash animation.
-      var svg = document.getElementById('button-clear-svg').style;
-      svg.animation = 'empty-trash-animation 0.3s';
-      svg.animationFillMode = 'forwards';
+      var svg = document.getElementById("button-clear-svg").style;
+      svg.animation = "empty-trash-animation 0.3s";
+      svg.animationFillMode = "forwards";
       //Show the previousOperand animation.
-      this.previousOperand.style.animation = 'hide-previous-value-animation 0.4s cubic-bezier(0.4, 1, 1, 1)';
-      this.previousOperand.style.animationFillMode = 'forwards';
-      setTimeout(() => {this.previousOperand.innerText = ""}, 200);
+      this.previousOperand.style.animation =
+        "hide-previous-value-animation 0.4s cubic-bezier(0.4, 1, 1, 1)";
+      this.previousOperand.style.animationFillMode = "forwards";
+      setTimeout(() => {
+        this.previousOperand.innerText = "";
+      }, 200);
       //Set operator state.
       this.currentOperand.innerText = "0";
       this.operationHasOperator = false;
@@ -25,32 +31,38 @@ class Calculator {
   }
 
   addNumber(element) {
-    if(this.currentOperand.innerText == "0") {
+    if (this.currentOperand.innerText == "0") {
       //Clear '0' to add new numbers.
       this.currentOperand.innerText = "";
       //Show trash animation.
-      var svg = document.getElementById('button-clear-svg').style;
-      svg.animation = 'trash-animation 0.3s';
-      svg.animationFillMode = 'forwards';
+      var svg = document.getElementById("button-clear-svg").style;
+      svg.animation = "trash-animation 0.3s";
+      svg.animationFillMode = "forwards";
     }
     //Check if the output can show properly the numbers.
-    if(this.currentOperand.innerText.length < 8) this.currentOperand.innerText += element.innerText;
+    if (this.currentOperand.innerText.length < 8)
+      this.currentOperand.innerText += element.innerText;
   }
 
   addOperator(element) {
     //Get the operator from the buttons
     var valueOperator = element.innerText;
     //Check if the user try to divide or multiply.
-    if(valueOperator == '%') valueOperator = '/';
-    if(valueOperator == 'x') valueOperator = '*';
+    if (valueOperator == "%") valueOperator = "/";
+    if (valueOperator == "x") valueOperator = "*";
 
     //add single operator.
-    if(this.currentOperand.innerText != "0" && this.previousOperand.innerText == '' && !this.operationHasOperator) {
+    if (
+      this.currentOperand.innerText != "0" &&
+      this.previousOperand.innerText == "" &&
+      !this.operationHasOperator
+    ) {
       //Calculate the operation.
       var operation = this.currentOperand.innerText + " " + valueOperator + " ";
       //Show the previousOperand animation.
-      this.previousOperand.style.animation = 'show-previous-value-animation 0.4s cubic-bezier(0.4, 1, 1, 1)';
-      this.previousOperand.style.animationFillMode = 'forwards';
+      this.previousOperand.style.animation =
+        "show-previous-value-animation 0.4s cubic-bezier(0.4, 1, 1, 1)";
+      this.previousOperand.style.animationFillMode = "forwards";
       //Display the result.
       this.previousOperand.innerText = operation;
       //Set operator state.
@@ -59,7 +71,7 @@ class Calculator {
     }
 
     //Add multiple operators.
-    if(this.operationHasOperator && this.currentOperand.innerText != '0') {
+    if (this.operationHasOperator && this.currentOperand.innerText != "0") {
       this.calculate(false);
       this.previousOperand.innerText += " " + valueOperator + " ";
       this.operationHasOperator = true;
@@ -67,21 +79,28 @@ class Calculator {
   }
 
   calculate(equalsButton = true) {
-    if(this.currentOperand.innerText != '0' && this.previousOperand.innerText != '') {
+    if (
+      this.currentOperand.innerText != "0" &&
+      this.previousOperand.innerText != ""
+    ) {
       //Sanitize the operation.
-      var operation = (this.previousOperand.innerText + this.currentOperand.innerText).replace(/[^-()\d/*+.]/g, '');;
+      var operation = (
+        this.previousOperand.innerText + this.currentOperand.innerText
+      ).replace(/[^-()\d/*+.]/g, "");
       //Calculate the operation.
       var result = eval(operation).toString().substr(0, 8);
       //Check if the user wants to continue the operation.
-      if(equalsButton) {
+      if (equalsButton) {
         //Show the result.
         this.currentOperand.innerText = result;
         //Show the previousOperand animation.
-        this.previousOperand.style.animation = 'hide-previous-value-animation 0.4s cubic-bezier(0.4, 1, 1, 1)';
-        this.previousOperand.style.animationFillMode = 'forwards';
-        setTimeout(() => {this.previousOperand.innerText = ""}, 200);
-      }
-      else {
+        this.previousOperand.style.animation =
+          "hide-previous-value-animation 0.4s cubic-bezier(0.4, 1, 1, 1)";
+        this.previousOperand.style.animationFillMode = "forwards";
+        setTimeout(() => {
+          this.previousOperand.innerText = "";
+        }, 200);
+      } else {
         //Continue operation.
         this.currentOperand.innerText = "0";
         this.previousOperand.innerText = result;
@@ -97,31 +116,35 @@ class Calculator {
  * @type {Array<HTMLElement>}
  */
 
-const operatorButtons = Array.from(document.getElementsByClassName('button-operator'));
+const operatorButtons = Array.from(
+  document.getElementsByClassName("button-operator")
+);
 /** A list with 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "." buttons of the document
  * @type {Array<HTMLElement>}
  */
 
-const numberButtons = Array.from(document.getElementsByClassName('button-number'));
+const numberButtons = Array.from(
+  document.getElementsByClassName("button-number")
+);
 /** The previous operation text
  * @type {HTMLElement}
  */
 
-const previousOperandText = document.getElementById('previousValueText');
+const previousOperandText = document.getElementById("previousValueText");
 /** The current operation text
  * @type {HTMLElement}
  */
 
-const currentOperandText = document.getElementById('currentValueText');
+const currentOperandText = document.getElementById("currentValueText");
 /** The equals button
  * @type {HTMLElement}
  */
 
-const equalsButton = document.getElementById('button-equals');
+const equalsButton = document.getElementById("button-equals");
 /** The clear button
  * @type {HTMLElement}
  */
-const clearButton = document.getElementById('button-clear');
+const clearButton = document.getElementById("button-clear");
 
 /** The main class:
  * - This contain al the functions to work
@@ -130,22 +153,22 @@ const clearButton = document.getElementById('button-clear');
 const calculator = new Calculator(previousOperandText, currentOperandText);
 
 //Add functions to the buttons of the document.
-operatorButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    calculator.addOperator(button)
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    calculator.addOperator(button);
   });
-})
+});
 
-numberButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    calculator.addNumber(button)
+numberButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    calculator.addNumber(button);
   });
-})
+});
 
-equalsButton.addEventListener('click', function() {
+equalsButton.addEventListener("click", function () {
   calculator.calculate();
-})
+});
 
-clearButton.addEventListener('click', function() {
+clearButton.addEventListener("click", function () {
   calculator.clear();
-})
+});
